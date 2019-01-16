6a21341ee0f28a30fcbcfa66a298de5730830ea6
dspverden <>
2019-01-16 22:49:19 +0100

    Added SigmaStudio files for 8 channel loudpseaker management

---
+++ SOURCES/SIGMASTUDIO/8channels/defines.h
@@ -0,0 +1,26 @@
+/*
+ * File:           defines.h
+ *
+ * Created:        Wednesday, October 31, 2018 4:30:26 PM
+ * Description:    8channels IC default download data definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2018 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+#define BufferSize_IC_1     	89554
+#define NumTransactions_IC_1	42
+
+
+#endif
