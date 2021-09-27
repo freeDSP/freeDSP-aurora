10ac06b0e365c02e5bf30029d1ae1ce945a91037
dspverden <>
2021-09-27 15:44:54 +0200

    Fixed issue #80

--- SOURCES/WEBAPP/ESP32/aurora/data/aurora.js
+++ SOURCES/WEBAPP/ESP32/aurora/data/aurora.js
@@ -620,8 +620,8 @@ function postJson(btp){
   else if(btp=="fir"){
     var idx=document.getElementById("fir").dataset.idx;
     var bypass=document.getElementById("fir_bypass").dataset.bypass;
-    var taps = new Float32Array(4096);
-    for(var k=0;k<4096;k++) taps[k]=0;
+    var taps = new Float32Array(20000);
+    for(var k=0;k<20000;k++) taps[k]=0;
     document.getElementById("msg").innerHTML="Updating FIRs, please wait...";
     document.getElementById("plzw").style.display="block";
     if(document.getElementById("irfile").files.length>0){
@@ -630,7 +630,7 @@ function postJson(btp){
         readFirFile(file).then(function(result){
           var lines = result.split('\n');
           for(var ll=0; ll<lines.length; ll++){
-            if(ll<4096){
+            if(ll<20000){
               val = parseFloat(lines[ll]);
               if(!isNaN(val)){taps[ll]=val;}
             }
